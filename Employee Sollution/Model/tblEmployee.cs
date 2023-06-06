4c314a135b696f8ab40e80034a534663cc3dcf0d
TRIFO <TRIFO@DESKTOP-RF6ORRA>
2023-06-07 00:47:31 +0600

    Add project files.

---
+++ Employee Sollution/Model/tblEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Sollution.Model
+{
+    public class tblEmployee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public string EmployeeName { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        [Index(IsUnique = true)]
+        public string EmployeeCode { get; set; }
+
+        public decimal EmployeeSalary { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public List<tblEmployeeAttendance> EmployeeAttendances { get; set; }
+
+    }
+}
---
+++ Employee Sollution/Model/tblEmployeeAttendance.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Sollution.Model
+{
+    public class tblEmployeeAttendance
+    {
+        [Key]
+        [Column(Order = 1)]
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public DateTime AttendanceDate { get; set; }
+
+        public bool IsPresent { get; set; }
+
+        public bool IsAbsent { get; set; }
+
+        public bool IsOffday { get; set; }
+
+        public tblEmployee Employee { get; set; }
+    }
+}
